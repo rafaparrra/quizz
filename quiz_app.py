7ad87bed1dcd2c5de8ec004c151b167fa3f515f0
rafaparrra <91946442+rafaparrra@users.noreply.github.com>
2025-05-14 18:52:37 +0200

    Update quiz_app.py

--- quiz_app.py
+++ quiz_app.py
@@ -18,7 +18,7 @@ def normalize_name(s):
 
 @st.cache_data
 def load_quiz_df():
-    path = Path(__file__).parent / 'Quizz_Completo_Actualizado.xlsx'
+    path = Path(__file__).parent / 'Quizz Completo.xlsx'
     df = pd.read_excel(path)
     if 'Asignatura' in df.columns:
         df['Asignatura'] = df['Asignatura'].fillna(method='ffill')
